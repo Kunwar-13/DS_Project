71a526250c8d474bc6eece71db289cb1feb1674c
Kunwar-13 <ayushpreetsingh20@gmail.com>
2024-08-12 15:22:24 -0400

    File Header

---
+++ project.cpp
@@ -0,0 +1,17 @@
+/*
+ * File: project.cpp
+ * Project: Parcel Management System with Hash Tables and Binary Search Trees
+ * Programmer: Kridhay Makwana, Ayushpreet
+ * First version: 08/10/2024
+ * Description: This program manages parcel data using hash tables and binary search trees,
+ *              allowing users to perform various searches and display operations.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+ // Disable specific compiler warnings
+#pragma warning(disable : 4996)
